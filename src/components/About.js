0b38f6cf754d20350a25fd0f5ea5bc90ef8e61ef
Christa Sparks <christa@Christas-MacBook-Pro.local>
2019-04-10 14:50:36 -0600

    add dripping effect from header to about sections

--- src/components/About.js
+++ src/components/About.js
@@ -3,20 +3,23 @@ import React from "react"
 import Astro from "../assets/astronauts/astro-8.svg"
 // 3 or 8
 import Drip from "../assets/PaintSplatter-22.svg"
+import GalaxyPic from "../images/galaxy-milkyway.jpg"
 
 export default () => (
   <div className="about">
     <div className="about__background" />
-    <Drip className="about__header-drip" />
+    <div className="about__header-drip" />
     <Astro className="about__astro" />
     <div className="about__details">
+        <div className="about__details-info">
       <h1 className="about__details-header">About me</h1>
       <p className="about__details-text">
-        Hi! I'm Christa Sparks, a Full Stack web developer who loves to explore
-        and experiment with technologies of all sorts. i',m always excited to
-        learn new things, and see what exciting things I can do with it. I'm
-        probably working on a new wacky portfolio site right now, for fun.
+        I'm Christa Sparks, a Full Stack web developer who loves to explore and
+        experiment with technologies of all sorts. I',m always excited to learn
+        new things. I'm probably working on a new wacky portfolio site right
+        now, for fun.
       </p>
+      </div>
     </div>
   </div>
 )
