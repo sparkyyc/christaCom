4a023dbb669194ea1f814741e65e0b85c48bd0b2
Christa Sparks <christa@Christas-MacBook-Pro.local>
2019-04-10 12:46:15 -0600

    base of about is done, adding drip effect now

--- src/components/About.js
+++ src/components/About.js
@@ -2,18 +2,20 @@ import React from "react"
 
 import Astro from "../assets/astronauts/astro-8.svg"
 // 3 or 8
+import Drip from "../assets/PaintSplatter-22.svg"
 
 export default () => (
   <div className="about">
     <div className="about__background" />
+    <Drip className="about__header-drip" />
     <Astro className="about__astro" />
     <div className="about__details">
       <h1 className="about__details-header">About me</h1>
       <p className="about__details-text">
-        Hi! I'm Christa Sparks, Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Error sint deserunt nisi quaerat doloribus, tempore ea
-        cupiditate illo ab ut deleniti eligendi quis animi magni, et commodi ex!
-        Aspernatur, temporibus!
+        Hi! I'm Christa Sparks, a Full Stack web developer who loves to explore
+        and experiment with technologies of all sorts. i',m always excited to
+        learn new things, and see what exciting things I can do with it. I'm
+        probably working on a new wacky portfolio site right now, for fun.
       </p>
     </div>
   </div>
