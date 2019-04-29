66b8b4bb9e7749ba0c086f08f21dbb1347352ed8
Christa Sparks <sparkyyc@gmail.com>
2019-04-29 15:39:45 -0600

    created new about me section that is responsive

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,8 @@
 import React from "react"
 
-import Astro from "../assets/astronauts/astro-8.svg"
 import Astro2 from "../assets/astronauts/Astronaut16.svg"
-
-import Planet from "../assets/aliens/alien-3.svg"
-
-import Drip from "../assets/PaintSplatter-22.svg"
-import GalaxyPic from "../images/galaxy-milkyway.jpg"
+import Robo1 from "../assets/robots.svg"
+import Robo2 from "../assets/swirlEyedRobot.svg"
 
 export default () => (
   <div className="about">
@@ -14,16 +10,24 @@ export default () => (
     <div className="about__background" />
     <div className="about__header-drip" />
     <Astro2 className="about__astro" />
-    {/* <Planet className="about__planet" /> */}
     <div className="about__details">
       <div className="about__details-info">
-        <h1 className="about__details-header">About</h1>
-        <p className="about__details-text">
-          I'm Christa Sparks, a Full Stack web developer who loves to explore
-          and experiment with technologies of all sorts. I'm always excited to
-          learn new things. I'm probably working on a new wacky site for fun
-          right now.
+        <h1 className="about__details-header">About Me</h1>
+        <h4 className="about__details-sub">I break things on the web</h4>
+        <Robo1 className="about__details-robo1" />
+        <p className="about__details-text about__details-text-1">
+          I design and build great digital experiences like websites and mobile
+          applications. My strongest language is JavaScript, and my usual tools
+          include Node.js, React.js, and GraphQL. I’d love to work with you on
+          your next project, so feel free to reach out.
         </p>
+        <p className="about__details-text about__details-text-2">
+          I moved to Boulder, CO  from Florida in 2018 after getting my degree in accounting
+          at FSU and working for a few years in finance. I loved being creative
+          too much to stick with just numbers, now I've combined my love of
+          tech, numbers, and creativity into a career I love.
+        </p>
+        <Robo2 className="about__details-robo2" />
       </div>
     </div>
   </div>
