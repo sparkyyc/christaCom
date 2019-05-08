f91a5c31241f76701b2bf632e055a284e124da23
Christa Sparks <sparkyyc@gmail.com>
2019-05-08 16:41:40 -0600

    removed helmet and imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Helmet } from 'react-helmet'
+// import { Helmet } from 'react-helmet'
 
 import About from "../components/About"
 import Container from "../components/Container"
